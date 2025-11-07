d32b1155bfb8e866990673e28bbf68d6256a53d1
Felipe Buitrago <flipe282000@gmail.com>
2025-11-06 19:10:45 -0500

    feat: add Testimonial model with CRUD operations and aggregation capabilities

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL,
+})
 
-const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
-export default db;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
+export default prisma
