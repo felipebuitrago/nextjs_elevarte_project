bae4b757c5b6c4c8da0bfa1a4e6daef9fe214d16
Felipe Buitrago <flipe282000@gmail.com>
2025-11-14 13:29:54 -0500

    fix: add logging for forwarded host and local environment in auth callback

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -18,6 +18,8 @@ export async function GET(request: Request) {
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host') // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === 'development'
+      console.log('Forwarded Host:', forwardedHost)
+      console.log('Is Local Environment:', isLocalEnv)
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`)
