c4d64666424f49dd2caa35e7a6ee195fdf962600
Felipe Buitrago <flipe282000@gmail.com>
2025-11-14 13:54:49 -0500

    fix: add console log for redirect URL in signInWithGoogle function

--- src/lib/auth-actions.ts
+++ src/lib/auth-actions.ts
@@ -4,14 +4,15 @@ import { redirect } from 'next/navigation'
 import { createClient } from '@/utils/supabase/server'
 
 export async function signInWithGoogle() {
+  console.log(`${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`);
   const supabase = await createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      queryParams: {
-        access_type: "offline",
-        prompt: "consent",
-      },
+      //queryParams: {
+      //  access_type: "offline",
+      //  prompt: "consent",
+      //},
       redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
     },
   });
